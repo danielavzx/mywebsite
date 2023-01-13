2d1ae1f59787a97b23b5b34b7c9b9d1ca902687d
danielavzx <dv2487@columbia.edu>
2023-01-13 11:46:31 -0500

    1/13
    Add little icons

--- components/HomePage.vue
+++ components/HomePage.vue
@@ -1,14 +1,24 @@
 <template>
     <div>
-         <h1 id = "myname">Daniela Velazquez</h1>
         <img id = "cam" src = "camera-png.webp" >
+        <img id = "cam2" src = "camera-png.webp" >
+        <img id = "cam3" src = "camera-png.webp" >
+        <img id = "cs1" src = "csicon.png" >
+        <img id = "cs2" src = "csicon.png" >
+        <img id = "cont1" src = "1266221-200.png">
+        <img id = "cont2" src = "1266221-200.png">
+        <img id = "code1" src = "png-coding-1.png">
+        <img id = "code2" src = "png-coding-1.png">
+        
+        
 
         <center>
+        <h1 id = "myname">Hi, I'm Daniela Velazquez!</h1>
         <p id = "pronouns">She/Her/Hers</p>
         <p id = "line">First-Gen Computer Science & Film</p>
         <br>
         <p id = "uni">Columbia University</p>
-        </br>
+     
 
         <img id = "pic" src = "IMG-8754.jpg" >
 
@@ -22,9 +32,15 @@
     
     @import 'https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&display=swap';
    
+    body{
+        background-color: #000;
+    }
+
     #myname{
-        font-size: 30px;
+        font-size: 60px;
         font-family: 'Playfair Display', serif;
+        color: white;
+        text-decoration: underline wavy cyan 10px; 
     }
 
     #pic{
@@ -34,7 +50,7 @@
         border-radius: 50%;
         padding: 5px;
         position: relative;
-        top: -250px;
+        top: -270px;
         
     }
 
@@ -43,6 +59,7 @@
         font-size: 20px;
         position: relative;
         top: 440px;
+        color: white;
     }
 
     #line{
@@ -50,6 +67,7 @@
         font-size: 28px;
         position: relative;
         top: 410px;
+        color: white;
     }
 
     #uni{
@@ -57,20 +75,93 @@
         font-size: 28px;
         position: relative;
         top: 305px;
+        color: white;
     }
 
     #cam{
-        position: relative;
+        position: absolute;
         top: 250px;
         width: 75px;
         height: 60px;
         transform: rotate(310deg);
-    
+        left: 400px;
     }
 
+    #cam2{
+        position: absolute;
+        top: 340px;
+        width: 75px;
+        height: 60px;
+        transform: rotate(40deg);
+        right: 300px;
+    }
 
+    #cam3{
+        position: absolute;
+        top: 220px;
+        width: 40px;
+        height: 30px;
+        transform: rotate(310deg);
+        right: 470px;
+    }
 
+    #cs1{
+        position: absolute;
+        top: 390px;
+        width: 70px;
+        height: 70px;
+        transform: rotate(30deg);
+        left: 200px;
+    }
+
+    #cs2{
+        position: absolute;
+        top: 190px;
+        width: 70px;
+        height: 70px;
+        transform: rotate(30deg);
+        right: 200px;
+    }
+
+    #cont1{
+        position: absolute;
+        top: 190px;
+        width: 70px;
+        height: 70px;
+        transform: rotate(340deg);
+        left: 100px;
+    }
+
+    #cont2{
+        position: absolute;
+        top: 450px;
+        width: 70px;
+        height: 70px;
+        transform: rotate(340deg);
+        right: 200px;
+    }
+
+    #code1{
+        position: absolute;
+        top: 430px;
+        width: 160px;
+        height: 100px;
+        transform: rotate(340deg);
+        right: 470px;
+    }
+
+     #code2{
+        position: absolute;
+        top: 430px;
+        width: 160px;
+        height: 100px;
+        transform: rotate(10deg);
+        left: 470px;
+    }
 
     
-   
+
+
+
+
 </style>
